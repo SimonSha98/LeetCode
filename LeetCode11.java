5f9c813c75b6e7685f55ea8b427fef7c302b7fe9
SimonSha98 <99288453+SimonSha98@users.noreply.github.com>
2022-06-30 22:01:26 -0400

    Create LeetCode11.java

---
+++ LeetCode11.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int left = 0, right = height.length - 1;
+        int max = Math.min(height[left], height[right]) * (right - left);
+        while(left < right){
+            if(height[left] < height[right])
+                left ++;
+            else
+                right --;
+            max = Math.max(max, Math.min(height[left], height[right]) * (right - left));
+        }
+        return max;
+    }
+}
