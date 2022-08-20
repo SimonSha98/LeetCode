b4912cdae3b276229e3ac0af0a5a22468debaa74
SimonSha98 <99288453+SimonSha98@users.noreply.github.com>
2022-08-19 21:31:29 -0400

    Create LeetCode253.py

---
+++ python/LeetCode253.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        rooms = []
+        intervals.sort(key = lambda x: x[0])
+        heapq.heappush(rooms, intervals[0][1])
+
+        for i in intervals[1:]:
+            if rooms[0] <= i[0]:
+                heapq.heappop(rooms)
+            heapq.heappush(rooms, i[1])
+
+        return len(rooms)
